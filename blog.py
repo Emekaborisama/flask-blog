c7585ef2a49ab9317955ded98aaef6ec05532427
keita <keita_broadwater@yahoo.com>
2016-07-14 20:55:22 -0700

    Database creation and initial controller

--- blog.py
+++ blog.py
@@ -1,1 +1,23 @@
 # Blog using flask, hosted on AWS
+# blog.py - controller
+
+# imports
+from flask import Flask, render_template, request, session, flash, redirect, \
+url_for, g
+import sqlite3
+
+# configuration
+
+DATABASE = 'blog.db'
+
+app = Flask(__name__)
+
+# pulls in app configuration by looking for uppercase variables
+app.config.from_object(__name__)
+
+# fuction used for connecting to the DATABASE
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+if __name__ == '__main__':
+    app.run(debug=True)
